bb04e6e247a1a78b7835a37f119aa14937e4b425
David Gengenbach <info@davidgengenbach.de>
2019-11-22 12:46:55 +0100

    Cleaned up deployment

--- upload.sh
+++ upload.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
+USER="$1"
+PASSWORD="$2"
+
+EXCLUDES="lib,images,images/pages,.git,node_modules,.idea"
+
+if [ ! -z "$USER" ]
+then
+  USER_PASSWORD="${USER}:${PASSWORD}@"
+fi
+
+URL="ftp://${USER_PASSWORD}art-and-function.de/sites/art-and-function/"
+
 pyftpsync \
   upload . \
-  ftp://art-and-function.de/sites/art-and-function/ \
-  --exclude "lib,images,images/pages,.git,node_modules,.idea" \
+  "$URL" \
+  --exclude "$EXCLUDES" \
   --resolve local \
-  --progress \
-  --quiet
+  --progress
