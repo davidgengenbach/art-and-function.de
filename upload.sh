e6ebefa7143fe7dbf3b2ac8f5f6a412b89ce548d
David Gengenbach <info@davidgengenbach.de>
2019-11-22 12:36:42 +0100

    :)

--- upload.sh
+++ upload.sh
@@ -3,7 +3,7 @@
 pyftpsync \
   upload . \
   ftp://art-and-function.de/sites/art-and-function/ \
-  --exclude "lib,images,.git" \
+  --exclude "lib,images,images/pages,.git,node_modules,.idea" \
   --resolve local \
   --progress \
   --quiet
